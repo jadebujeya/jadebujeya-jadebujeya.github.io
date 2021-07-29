649194b6e2efa968b68de23797ec3ba65f280757
jadebujeya <78665183+jadebujeya@users.noreply.github.com>
2021-07-29 13:06:53 +1000

    Add files via upload

---
+++ foot.js
@@ -0,0 +1,11 @@
+document.write(`
+<br>
+<footer>
+<a href = "#TOP";><p style="text-align:center; border:None; color:pink";>Back to Top</p></a>
+</footer>
+
+</body>
+</html>
+
+<!-- Air America Regular  -->
+`);
